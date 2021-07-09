3c7e34db05a59e5c4858bf658c30369d03da9b06
Pirmin Kalberer <pka@sourcepole.ch>
2021-07-09 09:07:14 +0200

    Log unreferenced resources

--- controllers/resources_controller.py
+++ controllers/resources_controller.py
@@ -1,11 +1,13 @@
 from collections import OrderedDict
 import math
 import requests
+import json
 from urllib.parse import urljoin
 from operator import itemgetter
 
 from flask import abort, flash, redirect, render_template, request, url_for
 from sqlalchemy.orm import joinedload
+from sqlalchemy.ext.declarative import DeclarativeMeta
 
 from .controller import Controller
 from forms import ResourceForm
@@ -552,6 +554,9 @@ class ResourcesController(Controller):
                             # the maps(parent) resources and didn't find
                             # any reference
                             continue
+            if res.not_referenced:
+                self.logger.info("Unreferenced resource: %s" % json.dumps(
+                    res, cls=AlchemyEncoder))
 
     def import_children(self, id):
         """Import child resources for a resource:
@@ -657,3 +662,22 @@ class ResourcesController(Controller):
             msg = "Could not import layers: %s" % e
             self.logger.error(msg)
             flash(msg, 'error')
+
+
+class AlchemyEncoder(json.JSONEncoder):
+
+    def default(self, obj):
+        if isinstance(obj.__class__, DeclarativeMeta):
+            # an SQLAlchemy class
+            fields = {}
+            for field in [x for x in dir(obj) if not x.startswith('_') and x != 'metadata']:
+                data = obj.__getattribute__(field)
+                try:
+                    json.dumps(data)
+                    fields[field] = data
+                except TypeError:
+                    fields[field] = None
+            # a json-encodable dict
+            return fields
+
+        return json.JSONEncoder.default(self, obj)
