e32b0a2469dedab4c8223534099ac66258e5e7c6
Mathias Walker <mwa@sourcepole.ch>
2022-05-05 10:56:43 +0200

    add 'config_editor' plugin for editing input configs 'tenantConfig.json' and 'config.json'

---
+++ plugins/config_editor/__init__.py
@@ -0,0 +1,20 @@
+import os
+
+from plugins.config_editor.controllers import ConfigsController
+
+
+name = "Config Editor"
+
+
+def load_plugin(app, handler):
+    # check required config
+    config = handler().config()
+    input_config_path = config.get('input_config_path')
+    if input_config_path is None or not os.path.isdir(input_config_path):
+        app.logger.error(
+            "Config Editor plugin: "
+            "Required config option 'input_config_path' is not set or invalid"
+        )
+
+    # create controller (including routes)
+    ConfigsController(app, handler)
---
+++ plugins/config_editor/controllers/configs_controller.py
@@ -0,0 +1,208 @@
+from collections import OrderedDict
+from datetime import datetime
+import os
+from shutil import copyfile
+
+from flask import flash, json, Markup, redirect, render_template, request, \
+    url_for
+from json.decoder import JSONDecodeError
+
+
+class ConfigsController():
+    """Controller for config editors"""
+
+    def __init__(self, app, handler):
+        """Constructor
+
+        :param Flask app: Flask application
+        :param TenantConfigHandler handler: Tenant config handler
+        """
+        app.add_url_rule(
+            "/config_editor", "config_editor", self.index, methods=["GET"]
+        )
+        app.add_url_rule(
+            "/config_editor/tenantConfig", "config_editor_edit_tenant_config",
+            self.edit_tenant_config, methods=["GET"]
+        )
+        app.add_url_rule(
+            "/config_editor/tenantConfig", "config_editor_update_tenant_config",
+            self.update_tenant_config, methods=["POST"]
+        )
+        app.add_url_rule(
+            "/config_editor/config", "config_editor_edit_qwc2_config",
+            self.edit_qwc2_config, methods=["GET"]
+        )
+        app.add_url_rule(
+            "/config_editor/config", "config_editor_update_qwc2_config",
+            self.update_qwc2_config, methods=["POST"]
+        )
+
+        self.templates_dir = "plugins/config_editor/templates"
+        self.logger = app.logger
+        self.handler = handler
+
+    def index(self):
+        """Show entry page."""
+        return render_template(
+            "%s/index.html" % self.templates_dir, title="Config Editor"
+        )
+
+    def edit_tenant_config(self):
+        """Show editor for tenantConfig.json."""
+        return self.edit_json_config(
+            'tenantConfig.json', url_for('config_editor_update_tenant_config')
+        )
+
+    def update_tenant_config(self):
+        """Update tenantConfig.json."""
+        return self.update_json_config(
+            'tenantConfig.json', url_for('config_editor_update_tenant_config')
+        )
+
+    def edit_qwc2_config(self):
+        """Show editor for config.json."""
+        return self.edit_json_config(
+            'config.json', url_for('config_editor_update_qwc2_config')
+        )
+
+    def update_qwc2_config(self):
+        """Update config.json."""
+        return self.update_json_config(
+            'config.json', url_for('config_editor_update_qwc2_config')
+        )
+
+    def edit_json_config(self, file_name, action_url):
+        """Show editor for a JSON config file.
+
+        :param str file_name: Config file name
+        :param str action_url: URL for form submit
+        """
+        try:
+            config_data = self.load_config_file(file_name)
+        except Exception as e:
+            flash(
+                Markup(
+                    "Could not read <b>%s</b>:"
+                    "<br><br>"
+                    "<pre>%s</pre>" %
+                    (file_name, e)
+                ),
+                'error'
+            )
+            return redirect(url_for('config_editor'))
+
+        title = "Edit %s" % file_name
+
+        return render_template(
+            "%s/editor.html" % self.templates_dir, title=title,
+            action=action_url, config_data=config_data
+        )
+
+    def update_json_config(self, file_name, action_url):
+        """Update a JSON config file.
+
+        :param str file_name: Config file name
+        :param str action_url: URL for form submit
+        """
+        try:
+            # update config file
+            config_data = request.values.get('config_data')
+            self.save_json_config_file(config_data, file_name)
+            flash("%s has been updated." % file_name, 'success')
+
+            return redirect(url_for('config_editor'))
+        except JSONDecodeError as e:
+            flash("Invalid JSON: %s" % e, 'error')
+        except Exception as e:
+            flash(
+                Markup(
+                    "Could not save <b>%s</b>:"
+                    "<br><br>"
+                    "<pre>%s</pre>" %
+                    (file_name, e)
+                ),
+                'error'
+            )
+
+        # return to editor and show errors
+        title = "Edit %s" % file_name
+
+        return render_template(
+            "%s/editor.html" % self.templates_dir, title=title,
+            action=action_url, config_data=config_data
+        )
+
+    def input_config_path(self):
+        """Get input_config_path from config or raise exception if not set."""
+        current_handler = self.handler()
+        input_config_path = current_handler.config().get('input_config_path')
+        if input_config_path is None:
+            raise RuntimeError(
+                "Required config option 'input_config_path' is not set"
+            )
+
+        return input_config_path
+
+    def load_config_file(self, file_name):
+        """Get contents of input config file as JSON string.
+
+        :param str file_name: Config file name
+        """
+        config_data = None
+
+        # read config file
+        config_file_path = ""
+        try:
+            config_file_path = os.path.join(
+                self.input_config_path(), self.handler().tenant, file_name
+            )
+            self.logger.info("Reading config file %s" % config_file_path)
+            with open(config_file_path, encoding='utf-8') as f:
+                config_data = f.read()
+        except Exception as e:
+            self.logger.error(
+                "Could not read config file %s:\n%s" % (config_file_path, e)
+            )
+            raise e
+
+        return config_data
+
+    def save_json_config_file(self, config_data, file_name):
+        """Save contents to input config file.
+
+        :param str config_data: Config contents as JSON string
+        :param str file_name: Config file name
+        """
+        config_file_path = ""
+        try:
+            # validate JSON
+            config = json.loads(config_data, object_pairs_hook=OrderedDict)
+
+            # get target path
+            config_path = os.path.join(
+                self.input_config_path(), self.handler().tenant
+            )
+            config_file_path = os.path.join(config_path, file_name)
+
+            # save backup of current config file
+            # as '<file_name>-YYYYmmdd-HHMMSS.bak'
+            timestamp = datetime.utcnow().strftime("%Y%m%d-%H%M%S")
+            backup_file_name = "%s-%s.bak" % (file_name, timestamp)
+            backup_file_path = os.path.join(config_path, backup_file_name)
+            self.logger.info(
+                "Saving backup of config file to %s" % backup_file_path
+            )
+            copyfile(config_file_path, backup_file_path)
+
+            # update config file
+            self.logger.info("Updating config file %s" % config_file_path)
+            with open(config_file_path, 'wb') as f:
+                # NOTE: do not reformat JSON string
+                f.write(config_data.encode('utf8'))
+        except JSONDecodeError as e:
+            raise e
+        except Exception as e:
+            self.logger.error(
+                "Could not save config file %s:\n%s" % (config_file_path, e)
+            )
+            raise e
