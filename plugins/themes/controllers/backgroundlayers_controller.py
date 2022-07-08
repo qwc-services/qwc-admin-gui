3d750b6a2805f19466ef202f8fb0dfaf29e39665
Benoît Blanc <benoit.blanc@oslandia.com>
2022-07-08 15:02:20 +0200

    Background controller : can create or update background layer

--- plugins/themes/controllers/backgroundlayers_controller.py
+++ plugins/themes/controllers/backgroundlayers_controller.py
@@ -3,6 +3,7 @@ import json
 from collections import OrderedDict
 
 from flask import flash, redirect, render_template, url_for
+from wtforms import ValidationError
 
 from plugins.themes.forms import WMSLayerForm, WMTSLayerForm
 from plugins.themes.utils import ThemeUtils
@@ -30,11 +31,19 @@ class BackgroundLayersController():
         )
         app.add_url_rule(
             "/themes/backgroundlayers/new/<string:type>",
-            "new_backgroundlayer", self.new, methods=["GET", "POST"]
+            "new_backgroundlayer", self.new, methods=["GET"]
         )
         app.add_url_rule(
             "/themes/backgroundlayers/create/<string:type>",
-            "create_backgroundlayer", self.create, methods=["GET", "POST"]
+            "create_backgroundlayer", self.create, methods=["POST"]
+        )
+        app.add_url_rule(
+            "/themes/backgroundlayers/edit/<int:index>",
+            "edit_backgroundlayer", self.edit, methods=["GET"]
+        )
+        app.add_url_rule(
+            "/themes/backgroundlayers/update/<int:index>",
+            "update_backgroundlayer", self.update, methods=["POST"]
         )
         app.add_url_rule(
             "/themes/backgroundlayers/delete/<int:index>",
@@ -56,103 +65,90 @@ class BackgroundLayersController():
     def new(self, type="wms"):
         """Show empty backgroundlayer form."""
 
+        form = self.create_form(type, None)
         if type == "wms":
-            form = WMSLayerForm()
             template = "%s/wmslayer.html" % self.template_dir
         elif type == "wmts":
-            form = WMTSLayerForm()
             template = "%s/wmtslayer.html" % self.template_dir
-        else:
-            flash("Type {0} is not supported.".format(type), "warning")
-            return redirect(url_for("backgroundlayers"))
+        action = url_for("create_backgroundlayer", type=type)
 
         form.thumbnail.choices = list(map(lambda x: (x, x), ThemeUtils.get_mapthumbs(self.app, self.handler)))
         return render_template(
-            template, title="Add background layer", type=type, form=form
+            template, title="Add background layer", action=action, type=type, form=form
         )
 
     def create(self, type="wms"):
         """Add backgroundlayer."""
 
-        if type == "wms":
-            form = WMSLayerForm()
-            template = "%s/wmslayer.html" % self.template_dir
-        elif type == "wmts":
-            form = WMTSLayerForm()
-            template = "%s/wmtslayer.html" % self.template_dir
-        else:
-            flash("Type {0} is not supported.".format(type), 'warning')
-            return redirect(url_for('backgroundlayers'))
+        form = self.create_form(type, None)
 
         form.thumbnail.choices = ThemeUtils.get_mapthumbs(self.app, self.handler)
 
         if form.validate_on_submit():
-            backgroundlayer = OrderedDict()
-            backgroundlayer["type"] = type
-            backgroundlayer["url"] = form.url.data
-            backgroundlayer["name"] = form.name.data
-            backgroundlayer["title"] = form.title.data
-            backgroundlayer["attribution"] = form.attribution.data
-            backgroundlayer["thumbnail"] = form.thumbnail.data
-
-            if type == "wms":
-                backgroundlayer["format"] = form.format.data
-                backgroundlayer["srs"] = form.srs.data
-                backgroundlayer["tiled"] = form.tiled.data
-                bbox = [float(x) for x in form.bbox.data.split(",")]
-                backgroundlayer["boundingBox"] = {
-                    "crs": form.srs.data,
-                    "bounds": bbox
-                }
-            elif type == "wmts":
-                backgroundlayer["url"] = backgroundlayer["url"].replace(
-                    "{Style}", form.style.data)
-                # TODO: tileMatrixPrefix ?
-                backgroundlayer["tileMatrixPrefix"] = ""
-                backgroundlayer["tileMatrixSet"] = form.tileMatrixSet.data
-                backgroundlayer["projection"] = form.projection.data
-                backgroundlayer["format"] = form.format.data                
-                backgroundlayer["requestEncoding"] = form.requestEncoding.data
-                backgroundlayer["style"] = form.style.data  
-                backgroundlayer["originX"] = float(form.originX.data)
-                backgroundlayer["originY"] = float(form.originY.data)
-                resolutions = [
-                    float(x) for x in form.resolutions.data.split(",")
-                ]
-                backgroundlayer["resolutions"] = resolutions
-                tilesize = [
-                    int(x) for x in form.tileSize.data.split(",")
-                ]
-                backgroundlayer["tileSize"] = tilesize
-
-                if form.with_capabilities.data:
-                    backgroundlayer["capabilities"] = json.loads(
-                        form.capabilities.data)
-
-            self.themesconfig["themes"]["backgroundLayers"].append(
-                backgroundlayer)
-
-            if ThemeUtils.save_themesconfig(self.themesconfig, self.app, self.handler):
-                message = "Background layer '{0}' was created.\
-                        ".format(backgroundlayer["title"])
-                flash(message, "success")
-            else:
-                message = "Could not save background layer '{0}'.\
-                        ".format(backgroundlayer["title"])
-                flash(message, "error")
-
-            return redirect(url_for("backgroundlayers"))
-
+            try:
+                self.create_or_update_backgroundlayer(type, form)
+                return redirect(url_for("backgroundlayers"))
+            except ValidationError:
+                flash("Could not create background layer {0}.".format(
+                    form.title.data), "warning")
         else:
             # TODO: form.errors
-            self.app.logger.error("Error adding backroundlayer: \
+            flash("Could not create background layer {0}. {1}".format(form.title.data, form.errors),
+                  "error")
+            self.app.logger.error("Error adding backgroundlayer: \
                                   {}".format(form.errors))
-            flash("Background layer could not be created: \
-                  {}".format(form.errors), "error")
 
-        return render_template(
-            template, title="Add background layer", type=type, form=form
-        )
+    def edit(self, index=None):
+        """Show edit backgroundlayer form.
+
+        :param int index: Backgroundlayer ID
+        """
+        # find background layer
+        backgroundlayer = self.find_backgroundlayer(index)
+
+        if backgroundlayer is not None:
+            # show validation errors
+            if backgroundlayer["type"] == "wms":
+                template = "%s/wmslayer.html" % self.template_dir
+            elif backgroundlayer["type"] == "wmts":
+                template = "%s/wmtslayer.html" % self.template_dir
+            form = self.create_form(type=backgroundlayer["type"], backgroundlayer=backgroundlayer)
+            title = "Edit background layer"
+            action = url_for("update_backgroundlayer", index=index)
+
+            return render_template(
+                template, title=title, type=backgroundlayer["type"], form=form, action=action,
+                index=index, method="POST"
+            )
+        else:
+            # theme not found
+            abort(404)
+
+    def update(self, index=None):
+        """Update existing background layer.
+
+        :param int index: Backgroundlayer ID
+        """
+        # find backgroundlayer
+        backgroundlayer = self.find_backgroundlayer(index)
+
+        if backgroundlayer is not None:
+            form = self.create_form(type=backgroundlayer["type"], backgroundlayer=None)
+
+            if form.validate_on_submit():
+                try:
+                    # update background layer
+                    self.create_or_update_backgroundlayer(backgroundlayer["type"], form, index=index)
+                    return redirect(url_for("backgroundlayers"))
+                except ValidationError:
+                    flash("Could not update background layer {0}.".format(
+                        form.title.data), "warning")
+            else:
+                flash("Could not update background layer {0}.".format(
+                      form.title.data), "warning")
+        else:
+            # backgroundlayer not found
+            abort(404)
 
     def delete(self, index=None):
         """Delete backgroundlayer."""
@@ -168,3 +164,180 @@ class BackgroundLayersController():
             else:
                 flash("Could not delete background layer.", "error")
         return redirect(url_for("backgroundlayers"))
+
+    def find_backgroundlayer(self, index=None):
+        """Find backgroundlayer by ID.
+
+        :param int index: Backgroundlayer ID
+        """
+        for i, item in enumerate(self.themesconfig["themes"]["backgroundLayers"]):
+            if i == index:
+                return item
+
+        return None
+
+    def create_form(self, type="wms", backgroundlayer=None):
+        """Return form with fields loaded from themesConfig.json.
+
+        :param object backgroundlayer: Optional backgroundlayer object
+        """
+        if type == "wms":
+            form = WMSLayerForm()
+            # template = "%s/wmslayer.html" % self.template_dir
+        elif type == "wmts":
+            form = WMTSLayerForm()
+            # template = "%s/wmtslayer.html" % self.template_dir
+        else:
+            flash("Type {0} is not supported.".format(type), 'warning')
+            return redirect(url_for('backgroundlayers'))
+
+        form.thumbnail.choices = ThemeUtils.get_mapthumbs(self.app, self.handler)
+
+        if backgroundlayer is None:
+            return form
+        else:
+            if "url" in backgroundlayer:
+                form.url.data = backgroundlayer["url"]
+
+            if "title" in backgroundlayer:
+                form.title.data = backgroundlayer["title"]
+
+            if "name" in backgroundlayer:
+                form.name.data = backgroundlayer["name"]
+
+            if "attribution" in backgroundlayer:
+                form.attribution.data = backgroundlayer["attribution"]
+
+            if "thumbnail" in backgroundlayer:
+                form.thumbnail.data = backgroundlayer["thumbnail"]
+
+            if type == "wms":
+                if "format" in backgroundlayer:
+                    form.format.data = backgroundlayer["format"]
+                if "srs" in backgroundlayer:
+                    form.srs.data = backgroundlayer["srs"]
+                if "tiled" in backgroundlayer:
+                    form.tiled.data = backgroundlayer["tiled"]
+                if "boundingBox" in backgroundlayer:
+                    bbox = ",".join([str(x) for x in backgroundlayer["boundingBox"]["bounds"]])
+                    form.bbox.data = bbox
+            elif type == "wmts":
+                # if form.style.data:
+                #     item["url"] = item["url"].replace(
+                #         "{Style}", form.style.data)
+                # TODO: tileMatrixPrefix ?
+                # item["tileMatrixPrefix"] = ""
+                if "tileMatrixSet" in backgroundlayer:
+                    form.tileMatrixSet.data = backgroundlayer["tileMatrixSet"]
+                if "projection" in backgroundlayer:
+                    form.projection.data = backgroundlayer["projection"]
+                if "originX" in backgroundlayer:
+                    form.originX.data = str(backgroundlayer["originX"])
+                if "originY" in backgroundlayer:
+                    form.originY.data = str(backgroundlayer["originY"])
+                if "resolutions" in backgroundlayer:
+                    resolutions = ",".join([str(x) for x in backgroundlayer["resolutions"]])
+                    form.resolutions.data = resolutions
+                if "tileSize" in backgroundlayer:
+                    tileSize = ",".join([str(x) for x in backgroundlayer["tileSize"]])                    
+                    form.tileSize.data = tileSize
+                if "capabilities" in backgroundlayer:
+                    form.with_capabilities.data = json.dumps(backgroundlayer["capabilities"])
+
+        return form
+
+    def create_or_update_backgroundlayer(self, type, form, index=None):
+        """Create or update backgroundlayer records in Themesconfig.
+
+        :param object backgroundlayer: Optional backgroundlayer object
+                                (None for create)
+        :param str type: Optional backgroundlayer object
+                                (None for create)
+        :param FlaskForm form: Form for backgroundlayer
+        """
+        item = OrderedDict()
+        item["type"] = type
+        item["url"] = form.url.data
+
+        if form.title.data:
+            item["title"] = form.title.data
+        else:
+            if "title" in item: del item["title"]
+
+        if form.name.data:
+            item["name"] = form.name.data
+        else:
+            if "name" in item: del item["name"]
+
+        item["attribution"] = ""
+        if form.attribution.data:
+            item["attribution"] = form.attribution.data
+
+        if form.thumbnail.data:
+            item["thumbnail"] = form.thumbnail.data
+
+        if type == "wms":
+            if form.format.data:
+                item["format"] = form.format.data
+            if form.srs.data:
+                item["srs"] = form.srs.data
+            if form.tiled.data:
+                item["tiled"] = form.tiled.data
+            if form.bbox.data:
+                bbox = [float(x) for x in form.bbox.data.split(",")]
+                item["boundingBox"] = {
+                    "crs": form.srs.data,
+                    "bounds": bbox
+                }
+        elif type == "wmts":
+            if form.style.data:
+                item["url"] = item["url"].replace(
+                    "{Style}", form.style.data)
+            # TODO: tileMatrixPrefix ?
+            item["tileMatrixPrefix"] = ""
+            if form.tileMatrixSet.data:
+                item["tileMatrixSet"] = form.tileMatrixSet.data
+            if form.projection.data:
+                item["projection"] = form.projection.data
+            if form.format.data:
+                item["format"] = form.format.data
+            if form.requestEncoding.data:
+                item["requestEncoding"] = form.requestEncoding.data
+            if form.style.data:
+                item["style"] = form.style.data
+            if form.originX.data:
+                item["originX"] = float(form.originX.data)
+            if form.originY.data:
+                item["originY"] = float(form.originY.data)
+            if form.resolutions.data:
+                resolutions = [
+                    float(x) for x in form.resolutions.data.split(",")
+                ]
+                item["resolutions"] = resolutions
+            if form.tileSize.data:
+                tilesize = [
+                    int(x) for x in form.tileSize.data.split(",")
+                ]
+                item["tileSize"] = tilesize
+            if form.with_capabilities.data:
+                item["capabilities"] = json.loads(
+                    form.capabilities.data)
+
+        # edit background layer
+        if index:
+            action_name = "updated"
+            self.themesconfig["themes"]["backgroundLayers"][index] = item
+        # new background layer
+        else:
+            action_name = "created"
+            self.themesconfig["themes"]["backgroundLayers"].append(
+                item)
+
+        if ThemeUtils.save_themesconfig(self.themesconfig, self.app, self.handler):
+            message = "Background layer '{0}' {1}.\
+                    ".format(item["title"], action_name)
+            flash(message, "success")
+        else:
+            message = "Could not save background layer '{0}'.\
+                    ".format(item["title"])
+            flash(message, "error")
